c567c0306637d0e1378be0094b6ab3667c7ad2da
evg-gornostai <harnastai750@gmail.com>
2022-05-08 17:04:30 +0300

    Added image loading

--- app/src/main/java/com/gornostai/rickandmorty/presentation/screens/characters/adapters/CharacterViewHolder.kt
+++ app/src/main/java/com/gornostai/rickandmorty/presentation/screens/characters/adapters/CharacterViewHolder.kt
@@ -1,7 +1,8 @@
 package com.gornostai.rickandmorty.presentation.screens.characters.adapters
 
 import androidx.recyclerview.widget.RecyclerView
-import com.gornostai.rickandmorty.R
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
 import com.gornostai.rickandmorty.databinding.ItemChatacterBinding
 import com.gornostai.rickandmorty.domain.entities.CharacterEntity
 
@@ -9,7 +10,15 @@ class CharacterViewHolder(val binding: ItemChatacterBinding) :
     RecyclerView.ViewHolder(binding.root) {
     fun bind(model: CharacterEntity) {
         binding.apply {
-            ivCharacterImage.setImageResource(R.drawable.ic_characters)
+            val circularProgressDrawable = CircularProgressDrawable(root.context)
+            circularProgressDrawable.strokeWidth = 5f
+            circularProgressDrawable.centerRadius = 30f
+            circularProgressDrawable.start()
+            Glide
+                .with(root.context)
+                .load(model.image)
+                .placeholder(circularProgressDrawable)
+                .into(ivCharacterImage)
             tvCharacterName.text = model.name
             tvCharacterGender.text = model.gender
             tvCharacterSpecies.text = model.species
